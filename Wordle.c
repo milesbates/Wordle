cd25e5538838a3c2414501bf9fae75ec795ac268
patrick7adams <adamsp@esdallas.org>
2023-01-23 09:40:11 -0600

    Added starter functions, comments

--- Wordle.c
+++ Wordle.c
@@ -3,3 +3,18 @@
 int main() {
     printf("hello");
 }
+
+void display() {
+    
+}
+
+int start_game() {
+    int num_letters;
+    printf("How many letters would you like to play with?");
+    scanf("%d", &num_letters);
+}
+
+// Fetch the initial word from a dictionary/json/database/something else.
+// Display the game in command line over time
+/* handle 5 letter words as input, see which letters are 
+ the same as the origin (green), present in other parts of the word (yellow) */
