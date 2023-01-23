59ad17eeceddfb9026769fa292762a3523331bb7
Simran <smalhotra2024@gmail.com>
2023-01-23 09:46:50 -0600

    description

--- Wordle.c
+++ Wordle.c
@@ -18,3 +18,6 @@ int start_game() {
 // Display the game in command line over time
 /* handle 5 letter words as input, see which letters are 
  the same as the origin (green), present in other parts of the word (yellow) */
+// check if each letter is in the right word and in the right position
+// if right but not in the right position, color letter yellow
+// if right and in right positition, color letter green
