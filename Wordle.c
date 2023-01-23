9a07354505ce9bf1c642925957e950bdd09db4b8
Simran <smalhotra2024@gmail.com>
2023-01-23 10:01:41 -0600

    Created method check_game, will add args later

--- Wordle.c
+++ Wordle.c
@@ -14,6 +14,11 @@ int start_game() {
     scanf("%d", &num_letters);
 }
 
+void check_game(){
+//checks each letter in array
+    
+}
+
 // Fetch the initial word from a dictionary/json/database/something else.
 // Display the game in command line over time
 /* handle 5 letter words as input, see which letters are 
@@ -21,3 +26,6 @@ int start_game() {
 // check if each letter is in the right word and in the right position
 // if right but not in the right position, color letter yellow
 // if right and in right positition, color letter green
+// make two arrays and check second array for each letter in the first array 
+// have six chances to guess the word before game ends 
+
